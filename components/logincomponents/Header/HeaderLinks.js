040f8c2ba8f9e0a350166a44453a5a08cb248f2b
Sylvest Shu <shaoqiangsssq@gmail.com>
2020-09-22 19:57:08 +0800

    Added login components and CSS styling

---
+++ components/logincomponents/Header/HeaderLinks.js
@@ -0,0 +1,48 @@
+/*eslint-disable*/
+import React from "react";
+import Link from "next/link";
+
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Tooltip from "@material-ui/core/Tooltip";
+import Icon from "@material-ui/core/Icon";
+
+// @material-ui/icons
+import { Apps, CloudDownload } from "@material-ui/icons";
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
+
+// core components
+import CustomDropdown from "components/logincomponents/CustomDropdown/CustomDropdown.js";
+import Button from "components/logincomponents/CustomButtons/Button.js";
+
+import styles from "assets/jss/nextjs-material-kit/components/headerLinksStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function HeaderLinks(props) {
+  const classes = useStyles();
+  return (
+    <List className={classes.list}>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="instagram-tooltip"
+          title="Follow us on instagram"
+          placement={"top"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            color="transparent"
+            href="https://www.instagram.com/CreativeTimOfficial?ref=creativetim"
+            target="_blank"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fab fa-instagram"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+    </List>
+  );
+}
---
+++ pages/admin/logintest.js
@@ -0,0 +1,18 @@
+import React from "react";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Icon from "@material-ui/core/Icon";
+// @material-ui/icons
+import Email from "@material-ui/icons/Email";
+import People from "@material-ui/icons/People";
+
+import Footer from "components/Footer/Footer.js";
+
+export default function logintest(props) {
+  return (
+    <div>
+        
+    </div>
+  );
+}
