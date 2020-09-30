cb23ceaeec6c828517adab3e9d1163d6aab541b0
Cheng Yang <e0203248@u.nus.edu>
2020-09-30 16:28:09 +0800

    register

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Router from "next/router";
-import Link from 'next/link';
+import Link from "next/link";
 
 export default class Index extends Component {
   componentDidMount = () => {
