87c906b2277cabe9b50aa8120746d142f185e0ac
Cheng Yang <e0203248@u.nus.edu>
2020-10-03 03:16:38 +0800

    fixed merge conflicts

--- redux/reducers/main.js
+++ redux/reducers/main.js
@@ -1,32 +1,32 @@
 import * as types from '../types';
 
-const main = (state = {
-    adminId: "",
-    name: "guest",
-    email: "",
-    adminType: "",
-    password: ""
-}, action) => {
-    switch(action.type) {
-        case types.SET_USER:
-            return {
-                adminId: action.payload.adminId,
-                name: action.payload.name,
-                email: action.payload.email,
-                adminType: action.payload.adminType,
-                password: action.payload.password
-            }
-        case types.LOGOUT: {
-            return {
-                adminId: "",
-                name: "guest",
-                email: "",
-                adminType: "",
-                password: ""
-            }
-        }
-        default:
-            return {...state}
+const main = (
+  state = {
+    adminId: '',
+    name: 'guest',
+    email: '',
+    adminType: '',
+    password: '',
+  },
+  action
+) => {
+  switch (action.type) {
+    case types.SET_USER:
+      return {
+        adminId: action.payload.adminId,
+        name: action.payload.name,
+        email: action.payload.email,
+        adminType: action.payload.adminType,
+        password: action.payload.password,
+      };
+    case types.LOGOUT: {
+      return {
+        adminId: '',
+        name: 'guest',
+        email: '',
+        adminType: '',
+        password: '',
+      };
     }
     default:
       return {...state};
