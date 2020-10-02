c53de599597703e7d1d5fcdaf70d3fbc3d692484
Cheng Yang <e0203248@u.nus.edu>
2020-10-02 09:59:08 +0800

    prettierrc.js

--- pages/admin/registerPage.js
+++ pages/admin/registerPage.js
@@ -1,42 +1,42 @@
-import React from "react";
+import React from 'react';
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import Icon from "@material-ui/core/Icon";
+import {makeStyles} from '@material-ui/core/styles';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Icon from '@material-ui/core/Icon';
 // @material-ui/icons
-import Email from "@material-ui/icons/Email";
-import People from "@material-ui/icons/People";
+import Email from '@material-ui/icons/Email';
+import People from '@material-ui/icons/People';
 // core components
-import Header from "components/logincomponents/Header/Header.js";
-import HeaderLinks from "components/logincomponents/Header/HeaderLinks.js";
-import GridContainer from "components/logincomponents/Grid/GridContainer.js";
-import GridItem from "components/logincomponents/Grid/GridItem.js";
-import Button from "components/logincomponents/CustomButtons/Button.js";
-import Card from "components/logincomponents/Card/Card.js";
-import CardBody from "components/logincomponents/Card/CardBody.js";
-import CardHeader from "components/logincomponents/Card/CardHeader.js";
-import CardFooter from "components/logincomponents/Card/CardFooter.js";
-import CustomInput from "components/logincomponents/CustomInput/CustomInput.js";
+import Header from 'components/logincomponents/Header/Header.js';
+import HeaderLinks from 'components/logincomponents/Header/HeaderLinks.js';
+import GridContainer from 'components/logincomponents/Grid/GridContainer.js';
+import GridItem from 'components/logincomponents/Grid/GridItem.js';
+import Button from 'components/logincomponents/CustomButtons/Button.js';
+import Card from 'components/logincomponents/Card/Card.js';
+import CardBody from 'components/logincomponents/Card/CardBody.js';
+import CardHeader from 'components/logincomponents/Card/CardHeader.js';
+import CardFooter from 'components/logincomponents/Card/CardFooter.js';
+import CustomInput from 'components/logincomponents/CustomInput/CustomInput.js';
 
-import styles from "assets/jss/nextjs-material-kit/pages/loginPage.js";
+import styles from 'assets/jss/nextjs-material-kit/pages/loginPage.js';
 
-import image from "assets/img/bg7.jpg";
-import Axios from "axios";
+import image from 'assets/img/bg7.jpg';
+import Axios from 'axios';
 
 const useStyles = makeStyles(styles);
 
 export default function RegisterPage(props) {
-  const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
+  const [cardAnimaton, setCardAnimation] = React.useState('cardHidden');
   setTimeout(function () {
-    setCardAnimation("");
+    setCardAnimation('');
   }, 700);
   const classes = useStyles();
-  const { ...rest } = props;
+  const {...rest} = props;
 
   async function register() {
     try {
       const response = await Axios.post(
-        "http://localhost:3000/admins/register",
+        'http://localhost:3000/admins/register',
         {
           name: name,
           email: email,
@@ -57,11 +57,10 @@ export default function RegisterPage(props) {
       <div
         className={classes.pageHeader}
         style={{
-          backgroundImage: "url(" + image + ")",
-          backgroundSize: "cover",
-          backgroundPosition: "top center",
-        }}
-      >
+          backgroundImage: 'url(' + image + ')',
+          backgroundSize: 'cover',
+          backgroundPosition: 'top center',
+        }}>
         <div className={classes.container}>
           <GridContainer justify="center">
             <GridItem xs={12} sm={6} md={4}>
@@ -78,7 +77,7 @@ export default function RegisterPage(props) {
                         fullWidth: true,
                       }}
                       inputProps={{
-                        type: "text",
+                        type: 'text',
                         endAdornment: (
                           <InputAdornment position="end">
                             <People className={classes.inputIconsColor} />
@@ -93,7 +92,7 @@ export default function RegisterPage(props) {
                         fullWidth: true,
                       }}
                       inputProps={{
-                        type: "email",
+                        type: 'email',
                         endAdornment: (
                           <InputAdornment position="end">
                             <Email className={classes.inputIconsColor} />
@@ -108,7 +107,7 @@ export default function RegisterPage(props) {
                         fullWidth: true,
                       }}
                       inputProps={{
-                        type: "password",
+                        type: 'password',
                         endAdornment: (
                           <InputAdornment position="end">
                             <Icon className={classes.inputIconsColor}>
@@ -116,7 +115,7 @@ export default function RegisterPage(props) {
                             </Icon>
                           </InputAdornment>
                         ),
-                        autoComplete: "off",
+                        autoComplete: 'off',
                       }}
                     />
                   </CardBody>
