9a6987191456d2ae8ee3ab1ae2b2b7159431a356
Cheng Yang <e0203248@u.nus.edu>
2020-10-03 02:42:57 +0800

    adminprofile + create admin

--- routes.js
+++ routes.js
@@ -16,63 +16,63 @@
 
 */
 // @material-ui/icons
-import Dashboard from "@material-ui/icons/Dashboard";
-import Person from "@material-ui/icons/Person";
-import LibraryBooks from "@material-ui/icons/LibraryBooks";
-import BubbleChart from "@material-ui/icons/BubbleChart";
-import LocationOn from "@material-ui/icons/LocationOn";
-import Notifications from "@material-ui/icons/Notifications";
+import Dashboard from '@material-ui/icons/Dashboard';
+import Person from '@material-ui/icons/Person';
+import LibraryBooks from '@material-ui/icons/LibraryBooks';
+import BubbleChart from '@material-ui/icons/BubbleChart';
+import LocationOn from '@material-ui/icons/LocationOn';
+import Notifications from '@material-ui/icons/Notifications';
 
 const dashboardRoutes = [
   {
-    path: "/dashboard",
-    name: "Dashboard",
+    path: '/dashboard',
+    name: 'Dashboard',
     icon: Dashboard,
 
-    layout: "/admin",
+    layout: '/admin',
   },
   {
-    path: "/admin-profile",
-    name: "Admin Profile",
+    path: '/admin-profile',
+    name: 'Admin Profile',
     icon: Person,
 
-    layout: "/admin",
+    layout: '/admin',
   },
-  {
-    path: "/table-list",
-    name: "Table List",
-    icon: "content_paste",
+  // {
+  //   path: "/table-list",
+  //   name: "Table List",
+  //   icon: "content_paste",
 
-    layout: "/admin",
-  },
-  {
-    path: "/typography",
-    name: "Typography",
-    icon: LibraryBooks,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/typography",
+  //   name: "Typography",
+  //   icon: LibraryBooks,
 
-    layout: "/admin",
-  },
-  {
-    path: "/icons",
-    name: "Icons",
-    icon: BubbleChart,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/icons",
+  //   name: "Icons",
+  //   icon: BubbleChart,
 
-    layout: "/admin",
-  },
-  {
-    path: "/maps",
-    name: "Maps",
-    icon: LocationOn,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/maps",
+  //   name: "Maps",
+  //   icon: LocationOn,
 
-    layout: "/admin",
-  },
-  {
-    path: "/notifications",
-    name: "Notifications",
-    icon: Notifications,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/notifications",
+  //   name: "Notifications",
+  //   icon: Notifications,
 
-    layout: "/admin",
-  },
+  //   layout: "/admin",
+  // },
 ];
 
 export default dashboardRoutes;
