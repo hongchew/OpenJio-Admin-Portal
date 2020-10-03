5946c37a0370e622641bc10bd5132fd7bdcac44b
Cheng Yang <e0203248@u.nus.edu>
2020-10-03 14:24:56 +0800

    save changes

--- redux/store.js
+++ redux/store.js
@@ -1,5 +1,9 @@
-import { createStore } from "redux";
-import rootReducer from "./reducers/rootReducer";
+import {createStore} from 'redux';
+import rootReducer from './reducers/rootReducer';
+import {loadState} from './localStorage';
 
-const store = createStore(rootReducer);
-export default store;
+// const store = createStore(rootReducer);
+
+const persistedState = loadState();
+
+export default createStore(rootReducer, persistedState);
